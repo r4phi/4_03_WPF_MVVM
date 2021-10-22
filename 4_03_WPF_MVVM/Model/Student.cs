d80fb0e0f6f98eaa35a442a920e34763218cfe1a
raphael.kapeller <raphael.kapeller@sz-ybbs.ac.at>
2021-10-22 13:15:11 +0200

    Projektdateien hinzufügen.

---
+++ 4_03_WPF_MVVM/Model/Student.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_03_WPF_MVVM.Model
+{
+    class Student : INotifyPropertyChanged
+    {
+        #region properties
+        private string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private int score;
+        public int Score
+        {
+            get => score;
+            set
+            {
+                score = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private DateTime timeAdded;
+        public DateTime TimeAdded
+        {
+            get => timeAdded;
+            set
+            {
+                timeAdded = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string comment = string.Empty;
+        public string Comment
+        {
+            get => comment;
+            set
+            {
+                comment = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region methods
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return Name.GetHashCode() * 3 + Score.GetHashCode() * 7;
+            }
+        }
+        public override bool Equals(object obj)
+        {
+            Student student = obj as Student;
+            if (student == null)
+                return false;
+
+            if (Name != student.Name)
+                return false;
+            if (Score != student.Score)
+                return false;
+            if (TimeAdded != student.TimeAdded)
+                return false;
+            if (Comment != student.Comment)
+                return false;
+
+            return true;
+        }
+        #endregion
+    }
+}
---
+++ 4_03_WPF_MVVM/Model/StudentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_03_WPF_MVVM.Model
+{
+    class StudentModel
+    {
+        #region properties
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+        #endregion
+
+        #region methods
+        public StudentModel()
+        {
+            // create some demo students
+            Students.Add(new Student { Name = "Mike", Score = 80 });
+            Students.Add(new Student { Name = "Alice", Score = 75 });
+        }
+
+        public void Add(Student studentToAdd)
+        {
+            // ToDo, Student darf nur hinzufgefügt werden, wenn er nicht null ist
+        }
+
+        public void Remove(Student studentToDelete)
+        {
+            // ToDo, Student darf nur gelöscht werden, wenn er nicht null ist
+        }
+        #endregion
+    }
+}
